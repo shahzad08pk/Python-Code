0d2a24bbb4d57f9712edce2a9c454ae7091b5846
Shahzad Nadeem <84832110+shahzad08pk@users.noreply.github.com>
2021-12-29 22:04:32 +0500

    Add files via upload

---
+++ arith.py
@@ -0,0 +1,6 @@
+a=50
+b=100
+print('sum of', a, 'and', b, 'is', a+b)
+print('Difference of', a, 'and', b, 'is', a-b)
+print('Product of', a, 'and', b, 'is', a*b)
+print('Division of', a, 'and', b, 'is', a/b)
